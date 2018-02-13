73e46c5a092c7b018d7f837834f63dd4073f0b9f
joope <pyry.joona@helsinki.fi>
2018-02-13 21:05:39 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,16 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    if (!blogs || blogs.length === 0) {
+        return 0;
+    }
+    return blogs.reduce((prev, blog) => {
+        return prev + blog.likes;
+    }, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
