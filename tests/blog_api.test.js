45a25bd55e636f7962b438ccff2668dc2a18e8d7
joope <pyry.joona@helsinki.fi>
2018-02-21 10:44:35 +0200

    4.12

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,33 +2,7 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-      },
-      {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-      },
-      {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-      }
-]
+const { format, initialBlogs, nonExistingId, blogsInDb } = require('./test_helper');
 
 beforeAll(async () => {
     await Blog.remove({});
@@ -60,15 +34,17 @@ describe('POST /api/blogs', () => {
             url: 'http://asdasdasd.com',
             likes: 2
         }
+        const blogsBefore = await blogsInDb();
+
         const response = await api
             .post('/api/blogs')
             .send(newBlog)
             .expect(201)
-        
-        expect(response.body.title).toEqual(newBlog.title)
-        expect(response.body.author).toEqual(newBlog.author)
-        expect(response.body.url).toEqual(newBlog.url)
-        expect(response.body.likes).toEqual(newBlog.likes)
+            
+        const blogsAfter = await blogsInDb();
+
+        expect(blogsAfter.length).toBe(blogsBefore.length + 1);
+        expect(blogsAfter).toContainEqual(newBlog);
     })
 
     test('posted blog without likes will default to 0 likes', async () => {
@@ -77,12 +53,20 @@ describe('POST /api/blogs', () => {
             author: 'Samuel Mikael Gabriel',
             url: 'http://asdasdasd.com',
         }
+        const blogsBefore = await blogsInDb();
+
         const response = await api
             .post('/api/blogs')
             .send(newBlog)
             .expect(201)
+
+        const blogsAfter = await blogsInDb();
         
-        expect(response.body.likes).toEqual(0)
+        expect(blogsAfter.length).toBe(blogsBefore.length + 1);
+        expect(blogsAfter).toContainEqual({
+            ...newBlog,
+            likes: 0
+        });
     })
 
     test('posted blog without title and url will fail', async () => {
